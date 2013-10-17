d1e8b4059a89a019883d89bbc13d8eef401f02a1
Colton Wolkins <frostyfrog2@gmail.com>
2013-10-16 23:40:05 -0600

    PEP8 compliance added

--- bin/cd.py
+++ bin/cd.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import sys
 import os
+
 aliases = {
-'scribes': '~/.Scribes/files/dev/' + 'e/i/e/i/o',
+    'scribes': '~/.Scribes/files/dev/' + 'e/i/e/i/o',
 }
+
 
 def traverse_path(path, debug=False):
     if debug:
@@ -11,22 +13,23 @@ def traverse_path(path, debug=False):
     if os.path.isdir(path):
         return path
     else:
-        return traverse_path(os.path.abspath(os.path.join(path, '..')),debug)
+        return traverse_path(os.path.abspath(os.path.join(path, '..')), debug)
+
 
 def arguments():
     import argparse
 
     # .:. Parser .:.
-    parser = argparse.ArgumentParser(prog='goto', usage='%(prog)s [-d] ALIAS',\
-            description=\
-"""Print out the location of an alias and go up the directory tree
-if the path doesn\'t exist.""")
+    parser = argparse.ArgumentParser(prog='gotoo', usage='%(prog)s [-d] ALIAS',
+                                     description="""\
+            Print out the location of an alias and go up the directory tree
+            if the path doesn\'t exist.""")
 
     # .:. Arguments .:.
-    parser.add_argument('-d', '--debug', action='store_true', required=False, \
-            help="Debug mode!")
-    parser.add_argument('alias', choices=aliases.keys(), type=str, \
-            help="Your special alias ^.^")
+    parser.add_argument('-d', '--debug', action='store_true', required=False,
+                        help="Debug mode!")
+    parser.add_argument('alias', choices=aliases.keys(), type=str,
+                        help="Your special alias ^.^")
 
     # .:. Return dem arrrrrrgs! .:.
     return parser.parse_args()
@@ -35,12 +38,12 @@ if __name__ == '__main__':
     args = arguments()
     if args.alias in aliases.keys():
         path = traverse_path(
-                os.path.abspath(
-                    os.path.expanduser(
-                        os.path.expandvars(aliases[args.alias])
-                        )
-                    ),
-                args.debug)
+            os.path.abspath(
+                os.path.expanduser(
+                    os.path.expandvars(aliases[args.alias])
+                    )
+                ),
+            args.debug)
         sys.stdout.write(path)
     else:
         sys.stderr('Error: Invalid alias!')
